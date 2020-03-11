5486dae05c628a10e3527e76dec92d12ab085bc6
Vlad Yandola <vladan2000.9@mail.ru>
2020-03-12 00:51:07 +0300

    Initial Commit

---
+++ NewsReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by Const. on 12.03.2020.
+//  Copyright © 2020 Oleginc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
